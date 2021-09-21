b6e73526a6b7f2c43e3fec0ff17ac67ed4b012cc
Abdallah-elsaede <abdallahmostapha957@gmail.com>
2021-09-21 14:28:03 +0200

    add live

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true , useUnifiedTopology : true});
+mongoose.connect(process.env.DATABASE_URL_LIVE, {useNewUrlParser: true , useUnifiedTopology : true});
 
 const DB = mongoose.connection;
 
